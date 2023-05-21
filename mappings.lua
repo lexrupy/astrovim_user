cca8d41b88f8a1dfacee0111682645fb0f7f5d7b
Alexandre da Silva <lexrupy@gmail.com>
2023-05-21 15:43:51 -0300

    fix typo

--- mappings.lua
+++ mappings.lua
@@ -10,7 +10,7 @@ return {
     -- mappings seen under group name "Buffer"
     ["<leader>bn"] = { "<cmd>tabnew<cr>", desc = "New tab" },
     ["<leader>bD"] = {
-      function )
+      function()
         require("astronvim.utils.status").heirline.buffer_picker(function(bufnr) require("astronvim.utils.buffer").close(bufnr) end)
       end,
       desc = "Pick to close",
