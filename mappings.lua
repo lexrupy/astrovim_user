75e4ced4ee09babb157042f1eb62f928d9164a30
Alexandre da Silva <lexrupy@gmail.com>
2023-06-26 02:54:31 -0300

    Added Surround to User settings

--- mappings.lua
+++ mappings.lua
@@ -18,6 +18,9 @@ return {
     -- tables with the `name` key will be registered with which-key if it's installed
     -- this is useful for naming menus
     ["<leader>b"] = { name = "Buffers" },
+    ["ç"] = {":"},
+    ["<Tab>"] = { ":bn<CR>", desc = "Next Buffer"},
+    ["<S-Tab>"] = { ":bp<CR>", desc = "Previous Buffer"},
     -- quick save
     -- ["<C-s>"] = { ":w!<cr>", desc = "Save File" },  -- change description but the same command
   },
---
+++ plugins/nvim-surround.lua
@@ -0,0 +1,10 @@
+return {
+  "kylechui/nvim-surround",
+  tag = "v2.1.0",
+  event = "VeryLazy",
+  config = function()
+    require("nvim-surround").setup({
+      --config here
+    })
+  end,
+}
