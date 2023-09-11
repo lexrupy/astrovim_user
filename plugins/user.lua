df1717e410c5dbd6213de888e409aa382f7433e6
Alexandre da Silva <lexrupy@gmail.com>
2023-09-11 11:25:29 -0300

    Config Updates

--- plugins/user.lua
+++ plugins/user.lua
@@ -18,24 +18,27 @@ return {
   --   opts = {},
   --   lazy = false
   -- },
-  -- {
-  --   "ggandor/leap.nvim",
-  --   enabled = true,
-  --   keys = {
-  --     { "s", mode = { "n", "x", "o" }, desc = "Leap forward to" },
-  --     { "S", mode = { "n", "x", "o" }, desc = "Leap backward to" },
-  --     { "gs", mode = { "n", "x", "o" }, desc = "Leap from windows" },
-  --   },
-  --   config = function(_, opts)
-  --     local leap = require("leap")
-  --     for k, v in pairs(opts) do
-  --       leap.opts[k] = v
-  --     end
-  --     leap.add_default_mappings(true)
-  --     vim.keymap.del({ "x", "o" }, "x")
-  --     vim.keymap.del({ "x", "o" }, "X")
-  --   end,
-  -- },
+  {
+    "ggandor/leap.nvim",
+    enabled = true,
+    dependencies = {
+      "tpope/vim-repeat"
+    },
+    keys = {
+      { "s", mode = { "n", "x", "o" }, desc = "Leap forward to" },
+      { "S", mode = { "n", "x", "o" }, desc = "Leap backward to" },
+      { "gs", mode = { "n", "x", "o" }, desc = "Leap from windows" },
+    },
+    config = function(_, opts)
+      local leap = require("leap")
+      for k, v in pairs(opts) do
+        leap.opts[k] = v
+      end
+      leap.add_default_mappings(true)
+      vim.keymap.del({ "x", "o" }, "x")
+      vim.keymap.del({ "x", "o" }, "X")
+    end,
+  },
   {
     "ThePrimeagen/vim-be-good",
     cmd = "VimBeGood",
